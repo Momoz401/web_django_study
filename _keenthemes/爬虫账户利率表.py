cd70e21c97150faa974a36127bd753c016c592b1
yangzhibin <yangzhibin401@icloud.com>
2024-01-30 00:30:00 +0800

    爬虫获取太平官网的账户利率

--- _keenthemes/爬虫账户利率表.py
+++ _keenthemes/爬虫账户利率表.py
@@ -1,25 +1,22 @@
 import requests as re
 from bs4 import BeautifulSoup
 
-response = re.get(url='https://life.cntaiping.com/pricehistory.jspx?a=1&productid=1353')
+response = re.get(url='https://life.cntaiping.com/pricehistory.jspx?a=1&productid=1353')    # 中国太平的账户利率公布
 
 if response.status_code == 200:
     html_content = response.text
-
     # 使用beautifulsoup 解析html
     soup = BeautifulSoup(html_content,'html.parser')
     price_div = soup.find('div', class_='price-an-div')
     if price_div:
         table_content = price_div.table
         if table_content:
-            rows = table_content.find_all('tr')
-            data_dict ={}
-            for row in rows[1:]:
-                tds = row.find_all(['th','td'])
-                key = tds[0].text.strip()
-                value = [tds[1].text.strip() for td in tds[1:]]
+            rows = table_content.find_all('tr') # 获取素有的行
+            data_dict = {}  # 声明一个空字典
+            for row in rows[1:]:  # 不需要第一行数据
+                tds = row.find_all(['th','td'])  #寻找列
+                key = tds[0].text.strip()  # 日期为key
+                value = [td.text.strip() for td in tds[1:]]  # 年利率为值
                 data_dict[key] = value
     print(data_dict)
-
-
 else:print(f"请求失败{response.status_code}")
